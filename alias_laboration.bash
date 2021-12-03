c0034a14f36178782adad04ebc5ee7bd5ea01731
Elliot Ozolins <45043033+elliotvadi@users.noreply.github.com>
2021-12-03 09:36:40 +0100

    Update alias_laboration.bash

--- alias_laboration.bash
+++ alias_laboration.bash
@@ -1,9 +1,17 @@
 #!bin/bash
 # Exempel på lösning på dagens laboration (03/12/2021)
 # Filen ska vara döpt till ~/.bashrc
+
+# Döper om 'clear' till cl:
 alias cl='clear'
+
+# Gör att kommandot 'hello' kör filen helloworld.bash
 alias hello='helloworld.bash'
+
+# Gör att '...' kan användas för att gå upp två steg i driectory
 alias ...='cd ../..'
+
+# Gör att 'pinggoogle' först skriver "Press Ctrl + C to break the operation" och pingar sedan Google
 alias pinggoogle='
 echo "Press Ctrl + C to break the operation"
 ping 8.8.8.8'
