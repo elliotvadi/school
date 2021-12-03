2e16ceff87add8c445cae59d61d4529b50b70b96
Elliot Ozolins <45043033+elliotvadi@users.noreply.github.com>
2021-12-03 09:33:57 +0100

    Add files via upload

---
+++ alias_laboration.bash
@@ -0,0 +1,10 @@
+#!bin/bash
+# Exempel på lösning på dagens laboration (03/12/2021)
+# Filen ska vara döpt till ~/.bashrc
+alias cl='clear'
+alias hello='helloworld.bash'
+alias ...='cd ../..'
+alias pinggoogle='
+echo "Press Ctrl + C to break the operation"
+ping 8.8.8.8'
+
